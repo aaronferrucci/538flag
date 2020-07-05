dfd2c87c31b6e2eb5f4286b2d06f69d5eee65732
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2020-07-04 22:57:45 -0700

    a bit of tidying up, and a useless graph

--- 538flag.R
+++ 538flag.R
@@ -4,25 +4,21 @@ lastprint <- 100
 done <- 0
 find <- 3
 for (i in 0:1000) {
-  flag <- ""
   ts <- 2*m*m
   cp <- (5*n*n + 5*n + 2) / 2
   # print any that are not larger
   if (ts <= cp) {
-    if (ts == lastprint + 1) 
-      flag <- " *"
-    print(sprintf("%d    twosquare(%d)%s", ts, m, flag), quote=F)
+    print(sprintf("%d    twosquare(%d)%s", ts, m, ifelse(ts == lastprint + 1, " *", "")), quote=F)
     m <- m + 1
     lastprint <- ts
   }
   if (cp <= ts) {
     if (cp == lastprint + 1) {
-      flag <- " +"
       find <- find - 1
       if (find == 0)
         done <- 1
     }
-    print(sprintf("%d    centpent(%d)%s", cp, n, flag), quote=F)
+    print(sprintf("%d    centpent(%d)%s", cp, n, ifelse(cp == lastprint + 1, " +", "")), quote=F)
     n <- n + 1
     lastprint <- cp
   }
@@ -30,3 +26,8 @@ for (i in 0:1000) {
   if (done)
     break
 }
+
+library(ggplot2)
+base <- ggplot() + xlim(0, 6)
+base + geom_function(fun=function(x) 2*x*x) + geom_function(fun=function(x) (5*x*x + 5*x + 2) / 2)
+
